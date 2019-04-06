33bb5a1c57924c55e0ba9a991836cf71445f984f
Praveen Kumar <praveen.kumar@appster.in>
2019-04-06 16:37:21 +0530

    contact-info offcampus done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,6 @@
 $(function() {
   //alert($.fn.jquery)
+  $('[data-toggle="offcanvas"]').on("click", function() {
+    $(".offcanvas-collapse").toggleClass("open");
+  });
 }); //End of document ready
