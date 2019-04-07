aac7a34dfd2be19d8b49a2a98e1da33caf62cfc6
Praveen Kumar <praveen.kumar@appster.in>
2019-04-07 18:04:15 +0530

    featured slick slider

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,43 @@ $(function() {
   $('[data-toggle="offcanvas"]').on("click", function() {
     $(".offcanvas-collapse").toggleClass("open");
   });
+
+  $(".products").slick({
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    mobileFirst: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 4
+        }
+      },
+      {
+        breakpoint: 1660,
+        settings: {
+          slidesToShow: 5
+        }
+      }
+
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
 }); //End of document ready
 
 var scene = document.getElementById("scene");
